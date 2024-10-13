999b907e606be50d298b97be83bedc315d1e63d7
Abir Hossen <abir.cse4@gmail.com>
2024-10-13 13:48:52 +0600

    time

--- ph-tube/script/test.js
+++ ph-tube/script/test.js
@@ -9,3 +9,15 @@ if (isVerified == true) {
 
 
 console.log(`${isVerified === true ? "Verified" : "Not verified"}`);
+
+
+function getTimeString(time){
+    const hour = parseInt(time / 3600);
+    let remainingSecond = time % 3600;
+    const minite = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+    return `${hour} hour ${minite} miniute ${remainingSecond} second ago`;
+}
+
+console.log(getTimeString(4505));
+
--- ph-tube/script/video.js
+++ ph-tube/script/video.js
@@ -1,4 +1,12 @@
 console.log('Added');
+// Time convert
+function getTimeString(time){
+    const hour = parseInt(time / 3600);
+    let remainingSecond = time % 3600;
+    const minite = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+    return `${hour} h ${minite} m ${remainingSecond} s ago`;
+}
 
 
 // Create LoadCategories
@@ -61,9 +69,14 @@ const displayVideos = (videos) => {
         const card = document.createElement('div');
         card.classList = "card card-compact"
         card.innerHTML = `
-            <figure class="h-[200px]">
+            <figure class="h-[200px] relative">
                 <img
                 src= ${video.thumbnail} class="h-full w-full object-cover" alt="Shoes" />
+
+                ${
+                    video.others.posted_date?.length == 0? "" : `<span class=" absolute right-2 bottom-2 bg-black rounded p-2 text-white">${getTimeString(video.others.posted_date)}</span>`
+                }
+                
             </figure>
             <div class="px-0 py-2 flex gap-2">
                 <div>
