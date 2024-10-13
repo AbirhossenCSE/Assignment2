129b292eb2ca4f3782f25e4bf3526d680ef9737d
Abir Hossen <abir.cse4@gmail.com>
2024-10-13 15:53:34 +0600

    button

--- ph-tube/script/video.js
+++ ph-tube/script/video.js
@@ -18,32 +18,6 @@ const loadCategories = () => {
     .catch((err) => console.log(err));
 }
 
-
-
-// category: "Music"
-// category_id: "1001"
-
-
-// Create displayCatagories
-const displayCategories = (categories) => {
-    const categoriesContainer = document.getElementById('categories');
-
-    categories.forEach((item)=> {
-        console.log(item);
-        
-        // Create a button
-        const button = document.createElement('button');
-        button.classList = 'btn';
-        button.innerText = item.category;
-
-        // add button at category container
-        categoriesContainer.appendChild(button);
-    });
-    
-}
-
-loadCategories();
-
 // Create LoadVideos
 const loadVideos = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
@@ -53,9 +27,71 @@ const loadVideos = () => {
     .catch((err) => console.log(err));
 }
 
+// Load CatagoriesVedon By 
+const loadCategoryVideos = (id) => {
+    // alert(id);
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json())
+    // .then((data) => console.log(data.categories))
+    .then((data) => displayVideos(data.category))
+    .catch((err) => console.log(err));
+}
+
+
+
+// category: "Music"
+// category_id: "1001"
+
+// Button
+// Create displayCatagories
+const displayCategories = (categories) => {
+    const categoriesContainer = document.getElementById('categories');
+
+    categories.forEach((item)=> {
+        console.log(item);
+        
+        // Create a button
+        const buttonContainer = document.createElement('button');
+        buttonContainer.innerHTML = `
+            <button onclick="loadCategoryVideos(${item.category_id})" class="btn">
+                ${item.category}
+            </button>
+        `
+
+        // add button at category container
+        categoriesContainer.appendChild(buttonContainer);
+    });
+    
+}
+
+// // Create displayCatagories
+// const displayCategories = (categories) => {
+//     const categoriesContainer = document.getElementById('categories');
+
+//     categories.forEach((item)=> {
+//         console.log(item);
+        
+//         // Create a button
+//         const button = document.createElement('button');
+//         button.classList = 'btn';
+//         button.innerText = item.category;
+
+//         // Buttin onClick
+//         // button.onclick = alert('Hello')
+
+//         // add button at category container
+//         categoriesContainer.appendChild(button);
+//     });
+    
+// }
+
+loadCategories();
+
+
+
 // authors: [{…}]
 // category_id: "1001"
-// description: "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// description: "Dive journey."
 // others: {views: '100K', posted_date: '16278'}
 // thumbnail: "https://i.ibb.co/L1b6xSq/shape.jpg"
 // title: "Shape of You"
@@ -64,6 +100,22 @@ const loadVideos = () => {
 // Create loadVideos section
 const displayVideos = (videos) => {
     const videoContainer = document.getElementById('videos')
+    videoContainer.innerHTML = "";
+
+    // No data
+    if (videos.length == 0) {
+        videoContainer.classList.remove("grid")
+        videoContainer.innerHTML = `
+            <div class=" min-h-[300px] w-full flex flex-col gap-5 justify-center items-center">
+                <img src="./assets/icon.png"/>
+                <h2 class=" text-center text-xl font-bold">No Content Here</h2>
+            </div>
+        `;
+    }
+    else{
+        videoContainer.classList.add("grid")
+    }
+
     videos.forEach((video) => {
         console.log(video);
         const card = document.createElement('div');
@@ -98,3 +150,5 @@ const displayVideos = (videos) => {
     
 }
 loadVideos()
+
+
