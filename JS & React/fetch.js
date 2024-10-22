46a75352c87d51e1cf52ccff1289de5b4265d481
Abir Hossen <abir.cse4@gmail.com>
2024-10-22 13:19:45 +0600

    json

---
+++ JS & React/fetch.js
@@ -0,0 +1,10 @@
+// 1. JSON
+const student = {
+    name: 'sakib khan',
+    age: 33,
+    movie: ['dhakar mastan', 'futpater pola']
+}
+const studentJSON = JSON.stringify(student);
+console.log(student);
+console.log(studentJSON);
+
