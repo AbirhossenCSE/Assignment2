cd6e993730d55f169465f9972aba1f65f0a167f3
Abir Hossen <abir.cse4@gmail.com>
2024-10-22 16:16:39 +0600

    notation

--- JS & React/naration.js
+++
@@ -1,6 +0,0 @@
-const person = {
-    anme: 'lal mia nol',
-    proffesion: 'Trafic serjon';
-    age: 34,
-    address: 'mirpur'
-}
---
+++ JS & React/notation.js
@@ -0,0 +1,19 @@
+const person = {
+    anme: 'lal mia nol',
+    proffesion: 'Trafic serjon',
+    age: 34,
+    address: 'mirpur',
+    25: 'summer'
+}
+
+// dot notation
+const prof1 = person.proffesion;
+console.log(prof1);
+
+// bracket notation
+const prof2 = person["proffesion"];
+console.log(prof2);
+
+const prof3 = person[25];
+console.log(prof3);
+
