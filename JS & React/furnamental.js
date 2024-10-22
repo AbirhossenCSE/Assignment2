419ae1b10779a68568b2448949be1e65eeb8f24f
Abir Hossen <abir.cse4@gmail.com>
2024-10-22 11:41:22 +0600

    6 basic js concept

--- JS & React/furnamental.js
+++ JS & React/furnamental.js
@@ -25,7 +25,29 @@ numbers[0] = 21;
 // 4. for loop
 for (let i = 0; i < numbers.length; i++) {
     const number = numbers[i];
-    console.log(number);
-    
-    
+    console.log(number); 
 }
+
+// 5. Function
+function multiply(num1, num2){
+    const result = num1 * num2;
+    return result;
+}
+const output = multiply(10, 12);
+console.log(output);
+
+// 6. Object
+// 3 ways to access proparty by name
+const student = {
+    name: 'sakib khan',
+    age: 33,
+    movie: ['king khan', 'dhakar mastan', 'futpater pola']
+}
+
+const myVeriable = 'age';
+console.log(student.age); //direct by proparty
+console.log(student['age']); //access via proparty name string
+console.log(student[myVeriable]); // access via proparty name in a veriable
+
+
+ 
