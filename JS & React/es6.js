c3b8ff3ca37abb2566086ad24a97b0c38cc3760a
Abir Hossen <abir.cse4@gmail.com>
2024-10-22 12:00:41 +0600

    arrow

---
+++ JS & React/es6.js
@@ -0,0 +1,23 @@
+const numbers = [45, 65, 42, 36];
+
+const student = {
+    name: 'sakib khan',
+    age: 33,
+    movie: ['king khan', 'dhakar mastan', 'futpater pola']
+}
+// 1. Template String
+const about = `My name is ${student.name}. I am ${student.age} years old. Has number of ${numbers[2]}. Also liked movies ${student.movie[2]}.`;
+console.log(about);
+
+// 2. Arrow Function
+const getFiftyFive = () => 55;
+const addSixtyFive = num => num + 65;
+const isEven = x => x % 2 === 0;
+const addThree = (x, y, z) => x + y + z;
+
+const doMath = (num1, num2) => {
+    const sum = num1 + num2;
+    return sum;
+}
+const output = doMath(12, 21);
+console.log(output);
