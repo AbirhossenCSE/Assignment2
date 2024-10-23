45875baf8ffb5fb09e4d8916816879787eb696db
Abir Hossen <abir.cse4@gmail.com>
2024-10-23 13:42:53 +0600

    conditional rendaring

--- explore-react/src/Todo.jsx
+++ explore-react/src/Todo.jsx
@@ -1,8 +1,8 @@
-// export default function Todo({task, isDone}){
-//     return (
-//         <li>Task: {task}</li>
-//     )
-// }
+export default function Todo({task, isDone}){
+    return (
+        <li>Task: {task}</li>
+    )
+}
 
 
 // Connditional Rendaring
@@ -17,4 +17,34 @@ export default function Todo({task, isDone}){
     }
 }
 
+// Connditional Rendaring 2
+export default function Todo({task, isDone}){
 
+    if (isDone) {
+        return <li>Finished: {task}</li>
+    }
+    return <li>Work One: {task}</li>  
+}
+
+// Connditional Rendaring 3: Ternary Operator
+export default function Todo({task, isDone}){
+    return (
+        <li>{isDone ? 'Finished' : 'work on' } {task}</li>
+    )
+}
+
+
+// Connditional Rendaring 4: &&
+export default function Todo({task, isDone}){
+    return (
+        <li>{task} {isDone && ': Done'}</li>
+    )
+}
+
+
+// Connditional Rendaring 5: ||
+export default function Todo({task, isDone}){
+    return (
+        <li>{task} {isDone || ': Do it'}</li>
+    )
+}
